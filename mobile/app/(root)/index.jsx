564c05f017481fa577ec9c65e21ab5588e76d4fd
V-M-B <vmbvarun@gmail.com>
2025-07-16 20:32:54 +0530

    feat: integrate transaction management into main app component
    - Added useTransactions hook to manage transaction data and summary.
    - Implemented useEffect to load transaction data on component mount.
    - Included console logs for debugging transaction and user ID information.

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -2,10 +2,19 @@ import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
 import { SignOutButton } from '../../components/SignOutButton'
+import { useTransactions } from '../../hooks/useTransactions'
+import {useEffect} from 'react'
 
 export default function Page() {
   const { user } = useUser()
+  const {transactions,summary,isLoading,loadData,deleteTransactions}=useTransactions(user.id)
 
+  useEffect(()=>{
+    loadData()
+  },[loadData])
+  console.log("transactions",transactions)
+  console.log("summary",summary)
+  console.log("userid",user.id)
   return (
     <View>
       <SignedIn>
